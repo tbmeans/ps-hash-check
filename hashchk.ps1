a9da0738aceb73c99932bb37f3391041fc4a159e
tbmeans <tbmeans2005@gmail.com>
2024-09-06 12:55:31 -0400

    clarified algo prompt

--- hashchk.ps1
+++ hashchk.ps1
@@ -1,7 +1,7 @@
 $path = Read-Host -Prompt "Enter absolute path of file to verify"
 $hash = Read-Host -Prompt "Enter checksum hash to verify against"
 $hash = $hash.ToUpper()
-$algo = Read-Host -Prompt "Enter algorithm of checksum hash"
+$algo = Read-Host -Prompt "Enter algorithm of checksum hash, no dashes"
 $algo = $algo.ToUpper()
 $is_g = (Get-FileHash $path -Algorithm $algo).Hash -eq $hash
 if ($is_g) {
